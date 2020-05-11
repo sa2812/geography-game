d7033cb2d862c0999d1e170b4887291aef96a8b2
Amrat <AMRATJX@legal.regn.net>
2020-05-11 21:11:37 +0100

    First part of the exercise:
    - Randomly display answers and options

--- game.py
+++ game.py
@@ -9,4 +9,26 @@ print(questions)
 
 # Write a function that displays the link to the image, and shows the four options in a random order
 def display_question(question):
-    # write code here
+    """
+    Next exercise:
+
+    Print questions in a random order, but also print which one is the correct answer and which is incorrect
+
+    Eg output:
+
+    Question
+    URL
+    Option 3 (incorrect)
+    Option 1 (incorrect)
+    Answer (correct)
+    Option 2 (incorrect)
+    """
+    print(question['question'])
+    print(question['url'])
+    
+    keys = question['options'] + [question['answer']]
+    random.shuffle(keys)
+    for key in keys:
+        print(key)
+
+display_question(questions[0])
