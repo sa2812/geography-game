c1912bcf2b77955d7b22eab197344a0fa231f33d
Sunny Amrat <amrat.sunny@gmail.com>
2020-05-11 20:19:39 +0100

    Added questions and task

--- game.py
+++ game.py
@@ -1,1 +1,12 @@
-print('Hello, world!')
+import json
+import random
+
+questions = []
+with open('questions.json') as json_file:
+    questions = json.load(json_file)
+
+print(questions)
+
+# Write a function that displays the link to the image, and shows the four options in a random order
+def display_question(question):
+    # write code here
